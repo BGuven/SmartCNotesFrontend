e63951544734935f9070ef5716c48152d10fc1e4
ahmedhawas <ahmedhawas7@gmail.com>
2015-03-25 18:26:45 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartCnotesFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the smartCnotesFrontendApp
+ */
+angular.module('smartCnotesFrontendApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartCnotesFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the smartCnotesFrontendApp
+ */
+angular.module('smartCnotesFrontendApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
