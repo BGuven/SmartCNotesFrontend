b5c58e47d7fd6e1fb7712b3c929edd123e039a37
ahmedhawas <ahmedhawas7@gmail.com>
2015-03-26 01:41:42 -0400

    add text box and search box

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,7 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/main', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         resolve:{
@@ -31,6 +31,6 @@ angular
         controller: 'AboutCtrl'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/main'
       });
   });
